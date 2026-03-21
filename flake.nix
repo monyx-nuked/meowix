0f2abf79e443e8a66f11a517b07c8007deb4a15f
monyx <helix.nuked@proton.me>
2026-03-21 05:05:20 +0000

    ➕ Add dependencies in flake Add inputs to the flake to have: nixpkgs, flake-parts, import-tree, nixpkgs-lib

--- flake.nix
+++ flake.nix
@@ -1,3 +1,22 @@
 {
-  outputs = { self }: { };
+  description = ''
+    meowix - nixos configuration w/
+        flake-parts and dendritic pattern'';
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+  inputs = {
+    # >> BASE <<
+    # nixpkgs - latest unstable version
+    nixpkgs.url = "https://flakehub.com/f/nixos/nixpkgs/0.1";
+    # flake-parts - flake framework
+    flake-parts = {
+      url = "https://flakehub.com/f/hercules-ci/flake-parts/*";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+    };
+    # import-tree - recursively import from directory
+    import-tree.url = "github:vic/import-tree";
+    # nixpkgs-lib - nixpkgs library
+    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+  };
 }
