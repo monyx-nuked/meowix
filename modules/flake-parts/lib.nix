fb0a99631867e22c6eec294c3bd8e2ad84b7e4cb
monyx-nuked <helix.nuked@proton.me>
2026-03-29 17:27:33 +0000

    fix(lib): fix wrongly cased functions

--- modules/flake-parts/lib.nix
+++ modules/flake-parts/lib.nix
@@ -5,17 +5,15 @@
   # > resolveHm - resolve module names for home-manager
   # > resolveDarwin - resolve module names for Darwin
   config.flake.lib = {
-    resolveOS =
-      module:
-      builtins.map (if builtins.isString module then config.flake.modules.nixos.${module} else module);
-    resolveHm =
-      module:
-      builtins.map (
-        if builtins.isString module then config.flake.modules.homeManager.${module} else module
-      );
-    resolveDarwin =
-      module:
-      builtins.map (if builtins.isString module then config.flake.modules.darwin.${module} else module);
+    resolveOS = builtins.map (
+      module: if builtins.isString module then config.flake.modules.nixos.${module} else module
+    );
+    resolveHm = builtins.map (
+      module: if builtins.isString module then config.flake.modules.homeManager.${module} else module
+    );
+    resolveDarwin = builtins.map (
+      module: if builtins.isString module then config.flake.modules.darwin.${module} else module
+    );
 
     # TODO: Research what is builtins.map and how it works
   };
