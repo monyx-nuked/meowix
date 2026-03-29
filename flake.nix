c0c7a041314280440e1ba3c7562611fee46f6aae
monyx-nuked <helix.nuked@proton.me>
2026-03-29 18:59:28 +0000

    chore(flake): add cores limit

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
     };
   };
   nixConfig = {
+    cores = 2; # Use 2 cores maximum for stability
     extra-trusted-public-keys = [
       "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
