cec89dd094f9a175ea60560c7af545598f52c345
monyx-nuked <helix.nuked@proton.me>
2026-03-29 12:21:25 +0000

    chore(devshell): add italic to motd, add more tools

--- modules/flake-parts/devshell.nix
+++ modules/flake-parts/devshell.nix
@@ -5,7 +5,7 @@
       devshells.default = {
         name = "nix-env";
         motd = ''
-          {202}Welcome to the {bold}nix-env!{reset}
+          {202}{italic}Welcome to the {bold}nix-env!{reset}
         '';
         packages = with pkgs; [
           statix # Linter
@@ -13,6 +13,10 @@
           nil # Language server
           nixd # Another language server
           deadnix # Scans for dead nix code
+          actionlint # Linter for github actions
+          shellcheck # Linter for shell scripts
+          shfmt # Formatter for shell scripts
+          # TODO: Add devenv and pre-commit hook
         ];
       };
     };
