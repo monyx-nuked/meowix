6b94de238131ebe475c3d2509377fdeee15761eb
monyx-nuked <helix.nuked@proton.me>
2026-03-30 11:03:57 +0000

    feat(i18n): add language settings

---
+++ modules/core/i18n.nix
@@ -0,0 +1,24 @@
+let
+  moduleName = "core/i18n";
+in
+{
+  flake.modules.nixos.${moduleName} = {
+    i18n.defaultLocale = "en_CA.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "uz_UZ.UTF-8";
+      LC_IDENTIFICATION = "uz_UZ.UTF-8";
+      LC_MEASUREMENT = "ru_RU.UTF-8";
+      LC_MONETARY = "en_CA.UTF-8";
+      LC_NAME = "en_CA.UTF-8";
+      LC_NUMERIC = "ru_RU.UTF-8";
+      LC_PAPER = "ru_RU.UTF-8";
+      LC_TELEPHONE = "uz_UZ.UTF-8";
+      LC_TIME = "uz_UZ.UTF-8";
+    };
+
+    services.xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+}
