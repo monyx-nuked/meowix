20076320ac93d35ebb3fec0ed56226b6a8369ce2
monyx-nuked <helix.nuked@proton.me>
2026-03-30 18:12:55 +0500

    feat: add disko and update lockfile

--- flake.nix
+++ flake.nix
@@ -58,6 +58,10 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # disko - declarative disk partitioning
+    disko = {
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   nixConfig = {
     cores = 2; # Use 2 cores maximum for stability
