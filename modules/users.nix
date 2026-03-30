f0695ca88b88b0ba2d8ef0e245124cef54ad6518
monyx-nuked <helix.nuked@proton.me>
2026-03-30 06:48:40 +0000

    chore(users): add mkForce to confirm

--- modules/users.nix
+++ modules/users.nix
@@ -4,19 +4,21 @@ let
 in
 {
   flake.modules = {
-    nixos.${moduleName} = {
-      users.users.${config.flake.meta.user.username} = {
-        isNormalUser = true;
-        description = config.flake.meta.user.fullName;
-        openssh.authorizedKeys.keys = config.flake.meta.user.ssh.sshPublicKeys;
-        extraGroups = [
-          "wheel"
-          "networkmanager"
-        ];
+    nixos.${moduleName} =
+      { lib, ... }:
+      {
+        users.users.${config.flake.meta.user.username} = {
+          isNormalUser = lib.mkForce true;
+          description = lib.mkForce config.flake.meta.user.fullName;
+          openssh.authorizedKeys.keys = lib.mkForce config.flake.meta.user.ssh.sshPublicKeys;
+          extraGroups = [
+            "wheel"
+            "networkmanager"
+          ];
+        };
       };
-    };
     homeManager.${moduleName} =
-      { pkgs, ... }:
+      { pkgs, lib, ... }:
       let
         inherit (config.flake.meta.user) avatar;
         avatarFile =
@@ -31,7 +33,7 @@ in
         programs.home-manager.enable = true;
         home = {
           inherit (config.flake.meta.user) username;
-          homeDirectory = "/home/${config.flake.meta.user.username}";
+          homeDirectory = lib.mkDefault "/home/${config.flake.meta.user.username}";
           stateVersion = "25.11";
 
           # Link avatar to .face for display managers
