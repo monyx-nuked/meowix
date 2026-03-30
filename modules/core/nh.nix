050b498c4a364163094aa8a2fdaefc22cc109753
monyx-nuked <helix.nuked@proton.me>
2026-03-30 18:02:52 +0500

    feat(nh): integrate to darwin

--- modules/core/nh.nix
+++ modules/core/nh.nix
@@ -19,4 +19,20 @@ in
         dix
       ];
     };
+  flake.modules.darwin.${moduleName} =
+    { pkgs, ... }:
+    {
+      programs.nh = {
+        enable = true;
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 7d --keep 10";
+        flake = "/Users/${config.flake.meta.user.username}/${config.flake.meta.user.github.repo}";
+      };
+
+      environment.systemPackages = with pkgs; [
+        nh
+        nix-output-monitor
+        dix
+      ];
+    };
 }
