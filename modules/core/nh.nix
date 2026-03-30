fe9ce6ebda57f96b535d59fc2b31de096265741c
monyx-nuked <helix.nuked@proton.me>
2026-03-30 11:04:40 +0000

    feat(nh): add nh tool with QoL settings

---
+++ modules/core/nh.nix
@@ -0,0 +1,22 @@
+{ config, ... }:
+let
+  moduleName = "core/nh";
+in
+{
+  flake.modules.nixos.${moduleName} =
+    { pkgs, ... }:
+    {
+      programs.nh = {
+        enable = true;
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 7d --keep 10";
+        flake = "/home/${config.flake.meta.user.username}/${config.flake.meta.user.github.repo}";
+      };
+
+      environment.systemPackages = with pkgs; [
+        nh
+        nix-output-monitor
+        dix
+      ];
+    };
+}
