eff465971b3ed78f8dbd77851566b1604a9b131b
monyx-nuked <helix.nuked@proton.me>
2026-04-03 00:42:52 +0500

    chore(host1): add metadata for this host

--- modules/hosts/meowscarada.nix
+++ modules/hosts/meowscarada.nix
@@ -2,9 +2,17 @@
 let
   prefix = "hosts.";
   host_name = "meowscarada";
+  host_meta = {
+    name = host_name;
+    ssh.alias = "wsl";
+    tailscale.enable = false;
+  };
 in
 {
-  flake.modules.nixos."${prefix}${host_name}" = {
-    system.stateVersion = "25.11";
+  flake = {
+    meta.hosts = [ host_meta ];
+    modules.nixos."${prefix}${host_name}" = {
+      system.stateVersion = "25.11";
+    };
   };
 }
