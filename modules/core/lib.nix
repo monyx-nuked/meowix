59390dea314188eff00de97d24f1856694884911
monyx-nuked <helix.nuked@proton.me>
2026-04-03 19:01:14 +0500

    feat(lib): implement resolve functions for hosts

---
+++ modules/core/lib.nix
@@ -0,0 +1,18 @@
+{ config, ... }:
+{
+  # Helper functions for host path resolving
+  # > resolve - resolve module names for NixOS
+  # > resolveHm - resolve module names for home-manager
+  # > resolveDarwin - resolve module name for Darwin
+  config.flake.lib = {
+    resolve = builtins.map (
+      mod: if builtins.isString mod then config.flake.modules.nixos.${mod} else mod
+    );
+    resolve_hm = builtins.map (
+      mod: if builtins.isString mod then config.flake.modules.home-manager.${mod} else mod
+    );
+    resolve_darwin = builtins.map (
+      mod: if builtins.isString mod then config.flake.modules.darwin.${mod} else mod
+    );
+  };
+}
