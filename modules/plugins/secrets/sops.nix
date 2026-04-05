51c703630cfc4a38ebf9f0f266f3f58a2f134bc4
monyx-nuked <helix.nuked@proton.me>
2026-04-06 00:06:52 +0500

    fix(sops): fix to string

--- modules/plugins/secrets/sops.nix
+++ modules/plugins/secrets/sops.nix
@@ -1,11 +1,14 @@
 { inputs, ... }:
+let
+  key_file = (toString inputs.self) + /secrets/key.txt;
+in
 {
   flake.modules = {
     nixos."secrets.sops" = {
       sops = {
-        defaultSopsFile = inputs.self + /secrets/secrets.yaml;
+        defaultSopsFile = inputs.self + /secrets/secrets.yaml; # TODO: Implement for each platform with different values ( if possible )
         defaultSopsFormat = "yaml";
-        age.keyFile = inputs.self + /secrets/key.txt;
+        age.keyFile = key_file;
         secrets = {
           user_password = {
             mode = "0400";
@@ -22,7 +25,7 @@
       sops = {
         defaultSopsFile = inputs.self + /secrets/secrets.yaml;
         defaultSopsFormat = "yaml";
-        age.keyFile = inputs.self + /secrets/key.txt;
+        age.keyFile = key_file;
         secrets = {
           # TODO: Add something
         };
@@ -32,7 +35,7 @@
       sops = {
         defaultSopsFile = inputs.self + /secrets/secrets.yaml;
         defaultSopsFormat = "yaml";
-        age.keyFile = inputs.self + /secrets/key.txt;
+        age.keyFile = key_file;
         secrets = {
           # TODO: Add something
         };
