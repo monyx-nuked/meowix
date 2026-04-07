4202b8b7e568683da60c60f45d69332ce68e374f
monyx-nuked <helix.nuked@proton.me>
2026-04-07 23:14:49 +0500

    feat(network): add network settings

---
+++ modules/core/system/network.nix
@@ -0,0 +1,27 @@
+{
+  flake.modules = {
+    nixos."system.network" =
+      { config, ... }:
+      {
+        # Disable wait-online
+        systemd.network.wait-online.enable = !config.networking.networkmanager.enable;
+        networking = {
+          networkmanager = {
+            insertNameservers = [
+              "1.1.1.1"
+              "1.0.0.1"
+            ];
+            wifi.backend = "iwd";
+            dhcp = "internal";
+          };
+          dhcpcd = {
+            inherit (config.networking.networkmanager) enable;
+          };
+          wireless.iwd = {
+            inherit (config.networking.networkmanager) enable;
+            settings.Settings.AutoConnect = true;
+          };
+        };
+      };
+  };
+}
