877da6c5ceecb07f77f37da84a2456098676c5bf
monyx-nuked <helix.nuked@proton.me>
2026-04-08 22:14:01 +0500

    fix(gemini): fix wrong config location

--- modules/plugins/dev/gemini.nix
+++ modules/plugins/dev/gemini.nix
@@ -1,15 +1,16 @@
-{ config, ... }:
 {
-  flake.modules.homeManager."dev.gemini" = {
-    programs.gemini-cli = {
-      enable = true;
-    };
+  flake.modules.homeManager."dev.gemini" =
+    { config, ... }:
+    {
+      programs.gemini-cli = {
+        enable = true;
+      };
 
-    sops.templates."gemini-env" = {
-      content = ''
-        GEMINI_API_KEY=${config.sops.placeholder."gemini_cli_api_key"}
-      '';
-      path = "${config.home.homeDirectory}/.config/environment.d/gemini.conf";
+      sops.templates."gemini-env" = {
+        content = ''
+          GEMINI_API_KEY=${config.sops.placeholder."gemini_cli_api_key"}
+        '';
+        path = "${config.home.homeDirectory}/.config/environment.d/gemini.conf";
+      };
     };
-  };
 }
